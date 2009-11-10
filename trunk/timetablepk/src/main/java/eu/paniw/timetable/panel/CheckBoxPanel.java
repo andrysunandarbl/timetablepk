5606f1971475fc9669fbfccc1bfdedcaef08601a
webgp.pl <webgp.pl@cfff1ce8-c4b8-11de-9e96-3193d784f102>
2009-11-10 06:57:40 +0000

    Add panels for edit page /for edit form/.

---
+++ trunk/timetablepk/src/main/java/eu/paniw/timetable/panel/CheckBoxPanel.java
@@ -0,0 +1,11 @@
+package eu.paniw.timetable.panel;
+
+import net.databinder.components.hib.DataPanel;
+
+
+public class CheckBoxPanel extends DataPanel {
+
+	public CheckBoxPanel() {
+		super("formfieldpanel");
+	}
+}
---
+++ trunk/timetablepk/src/main/java/eu/paniw/timetable/panel/PasswordPanel.java
@@ -0,0 +1,10 @@
+package eu.paniw.timetable.panel;
+
+import net.databinder.components.hib.DataPanel;
+
+public class PasswordPanel extends DataPanel {
+
+	public PasswordPanel() {
+		super("formfieldpanel");
+	}
+}
